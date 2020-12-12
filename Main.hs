6d38b5e8dc0c2bc9ffe308475541ee834c7b691b
Apoorv Kothari <apoorvko@amazon.com>
2020-12-11 16:19:56 -0800

    trying something

--- Main.hs
+++ Main.hs
@@ -1,4 +1,15 @@
 module Main where
+import System.IO
+
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let max_val = 0
+  contents <- readFile "1.txt"
+  let words_list = words contents
+  let mm = map readInt words_list
+  print max_val
+
+
+readInt :: String -> Int
+readInt = read
