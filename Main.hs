2fc51082a4b2ccec307d6a2285096aa9fa361d2f
Apoorv Kothari <apoorvko@amazon.com>
2020-12-14 00:14:41 -0800

    get pair and multiply.. next impl sum that match condition

--- Main.hs
+++ Main.hs
@@ -10,14 +10,21 @@ main :: IO ()
 main = do
   contents <- readFile "1.txt"
   let wordList = words contents
-  let intList = map readInt wordList
+  let intList = map readInt' wordList
   let maxVal = max' intList
-  print maxVal
+  let pair = getPair' intList
+  let prod = (\(x, y) -> x * y) pair
+  print prod
 
 
-readInt :: String -> Int
-readInt = read
+readInt' :: String -> Int
+readInt' = read
 
 max' :: [Int] -> Int
 max' (x:xs) = foldl (\acc curr -> if curr > acc then curr else acc) x xs
 
+getPair' :: [Int] -> (Int, Int)
+getPair' (x:xs) = (x, x)
+
+matchesSum' :: (Int, Int, Int) -> Bool
+matchesSum' (x, y, sum) = x + y == sum
